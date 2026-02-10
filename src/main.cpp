f4f0fbe2f3536426c18532bb24cdde4fd2fea9df
Rob Prouse <rob.prouse@aspiresoftware.com>
2026-02-09 19:06:59 -0700

    Add EditorConfig file and improve code formatting in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,42 +4,44 @@
 #define NUM_LEDS 1
 #define DATA_PIN 48
 
-#define BOOTLOG(...)             \
-  {                              \
-    char buf[120];               \
-    sprintf(buf, __VA_ARGS__);   \
-    Serial.print("##[BOOT]#\t"); \
-    Serial.println(buf);         \
-  }
+#define BOOTLOG(...)                 \
+    {                                \
+        char buf[120];               \
+        sprintf(buf, __VA_ARGS__);   \
+        Serial.print("##[BOOT]#\t"); \
+        Serial.println(buf);         \
+    }
 
 CRGB leds[NUM_LEDS];
 
-void setup() {
-  FastLED.addLeds<SK6812, DATA_PIN, GRB>(leds, NUM_LEDS);
-  Serial.begin(115200);
+void setup()
+{
+    FastLED.addLeds<SK6812, DATA_PIN, GRB>(leds, NUM_LEDS);
+    Serial.begin(115200);
 }
 
-void loop() {
-  leds[0] = CRGB::Red;
-  FastLED.show();
-  Serial.println("RED LED is ON");
-  delay(500);
-  leds[0] = CRGB::Yellow;
-  FastLED.show();
-  Serial.println("GREEN LED is ON");
-  delay(500);
-  leds[0] = CRGB::Blue;
-  FastLED.show();
-  Serial.println("BLUE LED is ON");
-  delay(500);
-  leds[0] = CRGB::Black;
-  FastLED.show();
-  Serial.println("LED's are OFF");
+void loop()
+{
+    leds[0] = CRGB::Red;
+    FastLED.show();
+    Serial.println("RED LED is ON");
+    delay(500);
+    leds[0] = CRGB::Yellow;
+    FastLED.show();
+    Serial.println("GREEN LED is ON");
+    delay(500);
+    leds[0] = CRGB::Blue;
+    FastLED.show();
+    Serial.println("BLUE LED is ON");
+    delay(500);
+    leds[0] = CRGB::Black;
+    FastLED.show();
+    Serial.println("LED's are OFF");
 
-  BOOTLOG("Total heap: %d", ESP.getHeapSize());
-  BOOTLOG("Free heap: %d", ESP.getFreeHeap());
-  BOOTLOG("Total PSRAM: %d", ESP.getPsramSize());
-  BOOTLOG("Free PSRAM: %d", ESP.getFreePsram());
+    BOOTLOG("Total heap: %d", ESP.getHeapSize());
+    BOOTLOG("Free heap: %d", ESP.getFreeHeap());
+    BOOTLOG("Total PSRAM: %d", ESP.getPsramSize());
+    BOOTLOG("Free PSRAM: %d", ESP.getFreePsram());
 
-  delay(1500);
+    delay(1500);
 }
