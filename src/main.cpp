eefff8e80a0d58413353c6200a5e42e216e1a194
Rob Prouse <rob.prouse@aspiresoftware.com>
2026-02-09 17:45:05 -0700

    Added a board file for the 8MB PSRAM

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,14 @@
 #define NUM_LEDS 1
 #define DATA_PIN 48
 
+#define BOOTLOG(...)             \
+  {                              \
+    char buf[120];               \
+    sprintf(buf, __VA_ARGS__);   \
+    Serial.print("##[BOOT]#\t"); \
+    Serial.println(buf);         \
+  }
+
 CRGB leds[NUM_LEDS];
 
 void setup() {
@@ -16,7 +24,7 @@ void loop() {
   FastLED.show();
   Serial.println("RED LED is ON");
   delay(500);
-  leds[0] = CRGB::Green;
+  leds[0] = CRGB::Yellow;
   FastLED.show();
   Serial.println("GREEN LED is ON");
   delay(500);
@@ -27,5 +35,11 @@ void loop() {
   leds[0] = CRGB::Black;
   FastLED.show();
   Serial.println("LED's are OFF");
+
+  BOOTLOG("Total heap: %d", ESP.getHeapSize());
+  BOOTLOG("Free heap: %d", ESP.getFreeHeap());
+  BOOTLOG("Total PSRAM: %d", ESP.getPsramSize());
+  BOOTLOG("Free PSRAM: %d", ESP.getFreePsram());
+
   delay(1500);
 }
